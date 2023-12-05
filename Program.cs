75f6d323514861bed5a7915288f567223931813b
juarika <lozadagomezj@gmail.com>
2023-12-04 19:30:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int opc = 0;
+        while (opc != 3)
+        {
+            Console.Clear();
+            Console.WriteLine("MENU RETO 5");
+            Console.WriteLine("1. SAM AND KELLY.");
+            Console.WriteLine("2. Subsequences of Array.");
+            Console.WriteLine("3. Salir.");
+            opc = Int32.Parse(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    SamAndKelly();
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Subsequences();
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.WriteLine("Hasta Luego");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+            }
+        }
+    }
+
+    public static void SamAndKelly()
+    {
+        Console.Write("SamDaily: ");
+        int samDaily = Int32.Parse(Console.ReadLine());
+        Console.Write("KellyDaily: ");
+        int kellyDaily = Int32.Parse(Console.ReadLine());
+        Console.Write("Difference: ");
+        int difference = Int32.Parse(Console.ReadLine());
+
+        int samSolved = samDaily + difference;
+        int kellySolved = kellyDaily;
+
+        if (samDaily >= kellyDaily)
+        {
+            Console.WriteLine(-1);
+        }
+
+        int day = 1;
+        while (kellySolved < samSolved)
+        {
+            samSolved += samDaily;
+            kellySolved += kellyDaily;
+            day++;
+        }
+
+        Console.WriteLine(day);
+    }
+
+    public static void Subsequences()
+    {
+        Console.Write("Array length: ");
+        int x = Int32.Parse(Console.ReadLine());
+        List<int> numberList = new();
+
+        for (int i = 1; i <= x; i++)
+        {
+            Console.Write($"Number {i}: ");
+            numberList.Add(Int32.Parse(Console.ReadLine()));
+        }
+
+        numberList.Sort();
+        bool isValid = IsValid(numberList);
+
+        if (isValid)
+        {
+            Console.WriteLine(numberList.Count);
+        }
+        else
+        {
+            for(int i = 0; i < numberList.Count; i++)
+            {
+                List<int> newList = new();
+                newList.AddRange(numberList);
+                isValid = IsValid(newList);
+                if (isValid)
+                {
+                    Console.WriteLine(newList.Count);
+                }
+            }
+        }
+    }
+
+    public static bool IsValid(List<int> values)
+    {
+        int sumOfDiff = 0;
+        for (int j = 1; j < values.Count; j++)
+        {
+            int difference = values[j] - values[j - 1];
+            sumOfDiff += difference;
+        }
+        return sumOfDiff % 2 == 0 ? true : false;
+    }
+}
